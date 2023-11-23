46c44cecbde950398a032e8cb22dde9cb43e8e23
Connor Espino <connorespino@u.boisestate.edu>
2023-11-23 16:43:22 -0700

    Started OSED Page

--- src/Pages/OSED.js
+++ src/Pages/OSED.js
@@ -5,16 +5,34 @@ import Footer from '../Components/Footer'
 
 function OSED() {
     return (
-        <div>
+        <div className='OSED'>
             <NavBar secondaryText='> OSED'/>
-            <iframe title='2018' width='1920' height='1080' src="https://player.vimeo.com/video/382834698">
-            </iframe>
-            <iframe title='2019' width='1920' height='1080' src="https://player.vimeo.com/video/383409297">
-            </iframe>
-            <iframe title='2020' width='1920' height='1080' src="https://player.vimeo.com/video/541100531">
-            </iframe>
-            <iframe title='2021' width='1920' height='1080' src="https://player.vimeo.com/video/661563462">
-            </iframe>
+            <div className='OSEDContainer'>
+                <div className='OSEDTitle'> One Second Every Day </div>
+                <div className='TimelineContainer'>
+                    <div className='Timeline'>
+
+                    </div>
+                    <div className='Frames'>
+                        <div className='FrameContainer'>
+                            <iframe className='VideoFrame' title='2018' src="https://player.vimeo.com/video/382834698" frameborder="0">
+                            </iframe>
+                        </div>
+                        <div className='FrameContainer'>
+                            <iframe className='VideoFrame' title='2019' src="https://player.vimeo.com/video/383409297" frameborder="0">
+                            </iframe>
+                        </div>
+                        <div className='FrameContainer'>
+                            <iframe className='VideoFrame' title='2020' src="https://player.vimeo.com/video/541100531" frameborder="0">
+                            </iframe>
+                        </div>
+                        <div className='FrameContainer'>
+                            <iframe className='VideoFrame' title='2021' src="https://player.vimeo.com/video/661563462" frameborder="0">
+                            </iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <Footer/>
         </div>
     );
