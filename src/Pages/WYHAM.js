9cbecf3eebff15c8bf54508c862dd6bc191a7614
Connor Espino <connorespino@u.boisestate.edu>
2023-12-30 00:16:54 -0700

    Added images to WYHAM (WIP)

--- src/Pages/WYHAM.js
+++ src/Pages/WYHAM.js
@@ -4,10 +4,29 @@ import TopComponent from '../Components/TopComponent';
 import Footer from '../Components/Footer'
 
 function WYHAM() {
+    const images = [
+        process.env.PUBLIC_URL + '/WYHAM Pictures/2021-03-06-0017.jpg',
+        process.env.PUBLIC_URL + '/WYHAM Pictures/2021-03-08-0014-2.jpg',
+        process.env.PUBLIC_URL + '/WYHAM Pictures/2021-05-29-0506-2-2.jpg',
+        process.env.PUBLIC_URL + '/WYHAM Pictures/2021-06-09-0514.jpg',
+        process.env.PUBLIC_URL + '/WYHAM Pictures/2021-08-14-0603.jpg',
+        process.env.PUBLIC_URL + '/WYHAM Pictures/2021-08-18-0014.jpg',
+        process.env.PUBLIC_URL + '/WYHAM Pictures/2021-08-18-0017.jpg',
+        process.env.PUBLIC_URL + '/WYHAM Pictures/2021-08-19-0001.jpg',
+        process.env.PUBLIC_URL + '/WYHAM Pictures/2021-12-04-0113.jpg',
+        process.env.PUBLIC_URL + '/WYHAM Pictures/2022-02-25-0111.jpg',
+        process.env.PUBLIC_URL + '/WYHAM Pictures/2022-02-25-0116.jpg',
+        process.env.PUBLIC_URL + '/WYHAM Pictures/2022-02-26-0116.jpg',
+        process.env.PUBLIC_URL + '/WYHAM Pictures/2022-02-26-0121.jpg',
+    ];
     return (
         <div>
             <TopComponent navSecondaryText='> WYHAM'/>
-            WYHAM
+            <div className='imageContainer'>
+                {images.map((item, index) => {
+                    return (<img src={item}></img>)
+                })}
+            </div>
             <Footer/>
         </div>
     );
